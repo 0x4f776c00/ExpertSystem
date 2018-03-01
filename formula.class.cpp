709723b5db12bb4ed24f5aebd8da25519affea1d
bcozic <bcozic@student.42.fr>
2018-03-01 15:40:05 +0100

    function or and xor

--- formula.class.cpp
+++ formula.class.cpp
@@ -6,7 +6,7 @@
 /*   By: bcozic <bcozic@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/01 15:03:33 by bcozic            #+#    #+#             */
-/*   Updated: 2018/03/01 15:21:17 by bcozic           ###   ########.fr       */
+/*   Updated: 2018/03/01 15:39:27 by bcozic           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,4 +27,30 @@ Formula::~Formula(void)
 void	Formula::compute_status()
 {
 	this->set_status(tab_func[this->relation](this->fact1, this->fact2));
+}
+
+e_status	or_operator(Facts fact1, Facts fact2)
+{
+	e_status	status1 = fact1.get_status();
+	e_status	status2 = fact2.get_status();
+
+	if (status1 == F_TRUE || status2 == F_TRUE)
+		return (F_TRUE);
+	if (status1 == F_FALSE && status2 == F_FALSE)
+		return (F_FALSE);
+	return (F_UNKNOWN);
+}
+
+e_status	xor_operator(Facts fact1, Facts fact2)
+{
+	e_status	status1 = fact1.get_status();
+	e_status	status2 = fact2.get_status();
+
+	if ((status1 == F_TRUE && status2 == F_FALSE)
+			|| (status1 == F_FALSE && status2 == F_TRUE))
+		return (F_TRUE);
+	if ((status1 == F_FALSE && status2 == F_FALSE)
+			|| (status1 == F_TRUE && status2 == F_TRUE))
+		return (F_FALSE);
+	return (F_UNKNOWN)
 }
