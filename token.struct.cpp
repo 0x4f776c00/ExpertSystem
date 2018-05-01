f89e6b3d261ea097605a5cd2a171d6b03f04c353
Ashbury <justasze@student.42.fr>
2018-05-01 18:12:33 +0200

    Some debugging required

--- token.struct.cpp
+++ token.struct.cpp
@@ -6,7 +6,7 @@
 /*   By: justasze <justasze@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/02 16:11:30 by bcozic            #+#    #+#             */
-/*   Updated: 2018/05/01 17:06:08 by justasze         ###   ########.fr       */
+/*   Updated: 2018/05/01 18:10:12 by justasze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,6 @@ Token::~Token()
 
 std::ostream & operator<<(std::ostream & o, const Token & token)
 {
-    o << "Symbol: '" << token.symbol << "' Type: " << static_cast<int>(token.type) << std::endl;
-    return o;
+	o << "Symbol: '" << token.symbol << "' Type: " << static_cast<int>(token.type) << std::endl << "operator_type: " << token.operator_type << std::endl;
+	return o;
 }
