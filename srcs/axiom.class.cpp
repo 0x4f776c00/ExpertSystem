4708c762f76615758632c3db05593070714c1142
bcozic <bcozic@student.42.fr>
2018-11-23 19:26:49 +0100

    fixe compute axiom

--- srcs/axiom.class.cpp
+++ srcs/axiom.class.cpp
@@ -6,7 +6,7 @@
 /*   By: bcozic <bcozic@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/01 14:31:33 by bcozic            #+#    #+#             */
-/*   Updated: 2018/11/23 18:45:39 by bcozic           ###   ########.fr       */
+/*   Updated: 2018/11/23 19:26:04 by bcozic           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,20 +27,15 @@ Axiom::~Axiom(void)
 int	Axiom::compute_axiom(bool testing)
 {
 	int	ret;
-	int	ret2;
 	e_status	status_before;
 
 	ret = this->fact1->compute_status(testing);
 	if (this->biconditional || this->fact1->get_status() == F_TRUE)
 	{
 		status_before = this->fact2->get_status();
-		ret2 = this->fact2->set_status(this->fact1->get_status(), testing);
-		// if (status_before != this->fact2->get_status())
-		// 	ret = ACTUALISED;
+		this->fact2->set_status(this->fact1->get_status(), testing);
+		if (status_before != this->fact2->get_status())
+			ret |= ACTUALISED;
 	}
-	if (ret == ERROR || ret2 == ERROR)
-		return ERROR;
-	if (ret == ACTUALISED || ret2 == ACTUALISED)
-		return ACTUALISED;
-	return NON_ACTUALISED;
+	return ret;
 }
